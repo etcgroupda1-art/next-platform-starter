5db1fa624806bb724ed930b3390a0a2efb4d80ea
etcgroupda1-art <etcgroup.da1@gmail.com>
2025-08-07 15:41:38 +0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Chào mừng bạn đến với AIMGVN!</h1>
+      <p>Đây là trang chủ Next.js được triển khai trên Netlify.</p>
+    </div>
+  );
+}
