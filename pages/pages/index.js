a7fbae09f97d66942ec927f71a447aaa1a689541
etcgroupda1-art <etcgroup.da1@gmail.com>
2025-08-07 15:36:29 +0700

    pages/index.js

---
+++ pages/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Chào mừng bạn đến với AIMGVN!</h1>
+      <p>Trang đang chạy trên Netlify + Next.js</p>
+    </div>
+  );
+}
